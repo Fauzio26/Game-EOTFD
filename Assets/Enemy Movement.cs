d49014104f6bb687ca49be6dc1a5cf132287057c
Fauzio Yunus Alim <fauzioyunusalim@gmail.com>
2026-06-21 14:06:25 +0700

    update tilemaps

--- Assets/Enemy Movement.cs
+++ Assets/Enemy Movement.cs
@@ -9,15 +9,19 @@ public class EnemyMovement : MonoBehaviour
     // [SerializeField] private Animator animator;
     [SerializeField] private float speed = 3f;
     [SerializeField] private int startDirection = 1;
+    [SerializeField] private bool stayOnLedges = true;
 
     private int currentDirection;
     private float halfWidht;
+    private float halfHeight;
     private Vector2 movement;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
        halfWidht = spriteRenderer.bounds.extents.x;
+       halfHeight = spriteRenderer.bounds.extents.y;
        currentDirection = startDirection; 
        spriteRenderer.flipX = startDirection == 1 ? false : true;
     }
@@ -31,15 +35,59 @@ public class EnemyMovement : MonoBehaviour
         setDirection();
     }
 
+    // PERBAIKAN: Huruf 'O' besar
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+        else{
+            isGrounded = false;
+        }
+    }
+
+    // PERBAIKAN: Diubah menjadi Exit agar tidak duplikat dengan fungsi di atasnya
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+
     private void setDirection()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.right, halfWidht + 0.1f, LayerMask.GetMask("Ground")) && rigidBody.velocity.x > 0){
-            currentDirection *= -1;
-            spriteRenderer.flipX = true;
+        if (!isGrounded) return;
+
+        Vector2 rightPos = transform.position;
+        Vector2 leftPos = transform.position;
+        rightPos.x += halfWidht;
+        leftPos.x -= halfWidht;
+
+        if (rigidBody.velocity.x > 0 )
+        {
+            if (Physics2D.Raycast(transform.position, Vector2.right, halfWidht + 0.1f, LayerMask.GetMask("Ground")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }
+            // PERBAIKAN: 0,1f menjadi 0.1f dan hapus titik dua (:) sebelum LayerMask
+            else if (stayOnLedges && !Physics2D.Raycast(rightPos, Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Ground")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }    
         }
-        else if (Physics2D.Raycast(transform.position, Vector2.left, halfWidht + 0.1f, LayerMask.GetMask("Ground")) && rigidBody.velocity.x < 0){
-            currentDirection *= -1;
-            spriteRenderer.flipX = false;
+        else if (rigidBody.velocity.x < 0)
+        {
+            if (Physics2D.Raycast(transform.position, Vector2.left, halfWidht + 0.1f, LayerMask.GetMask("Ground")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+             }
+             // PERBAIKAN: 0,1f menjadi 0.1f dan hapus titik dua (:) sebelum LayerMask
+             else if (stayOnLedges && !Physics2D.Raycast(leftPos, Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Ground")))
+             {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+             } 
         }
     }
 }
--- Assets/Player Jump.cs
+++ Assets/Player Jump.cs
@@ -53,15 +53,19 @@ public class PlayerJump : MonoBehaviour
             animator.SetBool("isJumping", false);
             animator.SetBool("isFalling", false);
         }
-        else if (yVel > 0.2f)
+        else
         {
-            animator.SetBool("isJumping", true);
-            animator.SetBool("isFalling", false);
-        }
-        else if (yVel < -0.8f)
-        {
-            animator.SetBool("isJumping", false);
-            animator.SetBool("isFalling", true);
+            // Angka disesuaikan agar lebih responsif mendeteksi naik/turun
+            if (yVel > 0.05f)
+            {
+                animator.SetBool("isJumping", true);
+                animator.SetBool("isFalling", false);
+            }
+            else if (yVel < -0.05f)
+            {
+                animator.SetBool("isJumping", false);
+                animator.SetBool("isFalling", true);
+            }
         }
     }
 
@@ -70,7 +74,7 @@ public class PlayerJump : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(
             transform.position,
             Vector2.down,
-            playerHalfHeight + 0.3f, // diperpanjang
+            playerHalfHeight + 0.05f, // Jarak raycast diperpendek agar deteksi tanah akurat
             LayerMask.GetMask("Ground")
         );
 
@@ -101,7 +105,7 @@ public class PlayerJump : MonoBehaviour
         Gizmos.DrawLine(
             transform.position,
             transform.position +
-            Vector3.down * (spriteRenderer.bounds.extents.y + 0.3f)
+            Vector3.down * (spriteRenderer.bounds.extents.y + 0.05f) // Gizmos disesuaikan dengan raycast baru
         );
     }
 }
